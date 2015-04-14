e6a100ea5f9c67f9c879d7de88a43af7174c6b12
Ally Dewar <Alastair@adewra.com>
2015-04-14 15:19:52 +0100

    Removed support for PHP 5.4

---
+++ tests/UpdatingTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Short description for class
+ *
+ * Long description for class (if any)...
+ *
+ * @package    Tinder
+ * @author     Ally Dewar <ally.dewar@adewra.com>
+ * @copyright  2015 Adewra Ltd
+ * @created      14/04/15 14:25
+ */
+
+namespace Adewra\Tinder;
+
+
+class UpdatingTest {
+
+}
